106d5b6090fd8a5c04730c1c45a5d660fee01e80
Roy Lowrance <roy.lowrance@gmail.com>
2015-03-23 21:41:10 -0400

    new

---
+++ dataset.py
@@ -0,0 +1,11 @@
+'''datasets
+
+SUMMARY OF APIs
+outliers(num_features, num_unique_samples)
+sparse(num_features, num_samples)
+'''
+import dataset_outliers
+outliers = dataset_outliers.outliers
+
+import dataset_sparse
+sparse = dataset_sparse.sparse
