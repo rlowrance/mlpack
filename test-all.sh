dd7b58fe57813895dfc794429916affaeb97e5ee
Roy Lowrance <roy.lowrance@gmail.com>
2015-03-24 10:48:56 -0400

    add module log

--- test-all.sh
+++ test-all.sh
@@ -12,9 +12,11 @@ python model_ridge.py
 python module_l2.py
 python module_linear.py
 python module_linear_n.py
+python module_log.py
 python module_negative_likelihood.py
 python module_softmax.py
 python module_squared.py
+python module_squared_vw.py
 python module_quantile.py
 python numeric_gradient.py
 python numpy_utilities.py
