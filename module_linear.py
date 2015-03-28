1c24319482ee85f98bb7f0a1d83070493eb1923a
Roy Lowrance <roy.lowrance@gmail.com>
2015-03-28 16:50:55 -0400

    redefine API for one sample only

--- module_linear.py
+++ module_linear.py
@@ -1,4 +1,13 @@
-'''linear with 1 output'''
+'''linear with 1 output
+
+FUNCTIONS
+output(theta, x)     -> number
+derivative(theta, x) -> vector
+
+ARGS
+theta: np array 1d
+x    : np array 1d
+'''
 import numpy as np
 import unittest
 import pdb
@@ -16,29 +25,34 @@ def linear():
         '''return number'''
         assert x.ndim == 1  # however, code works for x.ndim == 2
         b, w = split(theta)
-        return b + np.dot(x, w.T)
+        result = b + np.dot(x, w.T)  # this is an np.array
+        assert result.size == 1
+        return result[0]  # return a number
 
-    def gradient(x):
+    def gradient(theta, x):
         '''return np.array 1d with |theta| elements'''
+        assert theta.size == x.size + 1
         return np.hstack((1, x))
 
     return gradient, output
 
 
 class Test(unittest.TestCase):
+    def setUp(self):
+        self.x = np.array([4.0, -5])
+        self.theta = np.array([-1.0, 2, -3])
+
     def test_output(self):
         _, output = linear()
-        theta = np.array([-1, 2, -3])
-        x = np.array([4, -5])
-        actual = output(theta, x)
+        actual = output(self.theta, self.x)
+        self.assertTrue(isinstance(actual, float))
         expected = -1 + 2 * 4 - 3 * -5
         self.assertEqual(actual, expected)
 
     def test_gradient(self):
         gradient, _ = linear()
-        x = np.array([4, -5])
-        actual = gradient(x)
-        expected = np.hstack((1, x))
+        actual = gradient(self.theta, self.x)
+        expected = np.hstack((1, self.x))
         diff = np.linalg.norm(actual - expected)
         self.assertLess(diff, 1e-3)
 
