dbcd521cc479b05803f0a014f763b1312e51a048
Roy Lowrance <roy.lowrance@gmail.com>
2015-04-05 09:19:40 -0400

    replace osl and ridge with linear

--- model.py
+++ model.py
@@ -4,20 +4,10 @@ SUMMARY OF APIs
 multiclass(num_inputs, num_classes):
     gradient_loss(theta, x, y, probs) --> vector, number
     predict(theta, x) --> probs
-ols(num_inputs):
-    gradient(theta, x, y, [prediction]) -> vector
-    loss(theta, x, y, [prediction])     -> number
-    predict(theta, x)                   -> number
-ridge(num_inputs, regularizer_weights):
-    gradient(theta, x, y, [prediction]) -> vector
-    loss(theta, x, y, [prediction])     -> number
+linear(l1, l2):
+    gradient(theta, x, y [,prediction]) -> vector
+    loss(theta, x, y [,prediciton])     -> number
     predict(theta, x)                   -> number
 '''
-import model_multiclass
-multiclass = model_multiclass.multiclass
-
-import model_ols
-ols = model_ols.ols
-
-import model_ridge
-ridge = model_ridge.ridge
+from model_linear import linear
+from model_multiclass import multiclass
