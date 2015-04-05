6e57f85046f6d6b8517d0d4526652e2a49ed9b77
Roy Lowrance <roy.lowrance@gmail.com>
2015-04-05 08:44:34 -0400

    documentation

--- model.py
+++ model.py
@@ -5,11 +5,13 @@ multiclass(num_inputs, num_classes):
     gradient_loss(theta, x, y, probs) --> vector, number
     predict(theta, x) --> probs
 ols(num_inputs):
-    gradient_loss(x, y, predicted) --> vector, number
-    predict(theta, x)
+    gradient(theta, x, y, [prediction]) -> vector
+    loss(theta, x, y, [prediction])     -> number
+    predict(theta, x)                   -> number
 ridge(num_inputs, regularizer_weights):
-    gradient_loss(theta, x, y, prediction) --> vector, number
-    predict(theta, x) --> number
+    gradient(theta, x, y, [prediction]) -> vector
+    loss(theta, x, y, [prediction])     -> number
+    predict(theta, x)                   -> number
 '''
 import model_multiclass
 multiclass = model_multiclass.multiclass
