931b1846dc342e51a2b0a75175f21e67556d26b8
Roy Lowrance <roy.lowrance@gmail.com>
2015-04-06 19:05:32 -0400

    generate directly instead of using function

--- dataset_bishop_ch1.py
+++ dataset_bishop_ch1.py
@@ -1,22 +1,31 @@
 '''dataset from bishop-06 chapter 1'''
 import math
+import numpy as np
 import pdb
 import unittest
 
 
-from dataset_d1 import d1
+import random_sample
 
 
-def bishop_ch1(num_samples):
+def bishop_ch1(num_samples, error_variance):
     def fun(x):
         return math.sin(2.0 * math.pi * x)
 
-    x_low = 0
-    x_high = 1
+    x_values = np.linspace(start=0.0,
+                           stop=1.0,
+                           num=num_samples,
+                           endpoint=True,
+                           retstep=False)
+
     error_mean = 0
-    error_variance = 0.1
-    return \
-        d1(fun, num_samples, x_low, x_high, error_mean, error_variance)
+    x = np.zeros((num_samples, 1))
+    y = np.zeros(num_samples)
+    for i in xrange(num_samples):
+        x[i] = x_values[i]
+        error_vector = next(random_sample.randn(1, error_mean, error_variance))
+        y[i] = fun(x[i][0]) + error_vector[0]
+    return x, y
 
 
 class Test(unittest.TestCase):
@@ -25,7 +34,7 @@ class Test(unittest.TestCase):
         self.verbose = False
 
     def test(self):
-        x, y = bishop_ch1(self.num_samples)
+        x, y = bishop_ch1(self.num_samples, 0.1)
         if self.verbose:
             for i in xrange(self.num_samples):
                 print x[i][0], y[i]
